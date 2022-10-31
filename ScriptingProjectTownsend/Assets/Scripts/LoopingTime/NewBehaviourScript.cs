dfc1f6fa86ab8d653f45e33c5315796374fe1c71
Bribri18 <607897@student.dusd.net>
2022-10-31 09:59:30 -0700

    I created a Looping Script
    A script was created

---
+++ ScriptingProjectTownsend/Assets/Scripts/LoopingTime/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+
+        {
+            Print("Hello World")
+
+        } while (shouldContinue == true);
+
+    }
+
+}
+    
