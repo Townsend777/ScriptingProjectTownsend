6251a774cd02f86a725803aac4e60275629714fc
Bribri18 <607897@student.dusd.net>
2022-11-30 09:57:36 -0800

    I fixed a script
    I fixed a script

--- ScriptingProjectTownsend/Assets/Scripts/UpdateandFixedUpdate/UpdateandFixedUpdate.cs
+++ ScriptingProjectTownsend/Assets/Scripts/UpdateandFixedUpdate/UpdateandFixedUpdate.cs
@@ -13,6 +13,6 @@ public class UpdateandFixedUpdate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update Time :" + Time.deltaTime);
+        Debug.Log("Update time :" + Time.deltaTime);
     }
 }
