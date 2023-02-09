63378d6c3123bb8c4fdd28f9253f775b0360a8f2
Bribri18 <122388718+Townsend777@users.noreply.github.com>
2023-02-09 08:39:12 -0800

    IDK
    IDK

---
+++ ScriptingProjectTownsend/Assets/Scripts/Linear Interpolation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+     // In this case, result = 4
+        float result = Mathf.Lerp(3f, 5f 0.5f); 
+      // Math.Lerp takes 3 parameters and uses them to move the object around for example if it was 0 the function would return the from value  
+
+    Vector3 from = new Vector3 (1f, 2f, 3f); 
+    Vector3 to = new Vector3 (5f, 6f, 7f); 
+
+    // Here result = (4, 5, 6) 
+    Vector3 result = Vector3.Lerp (from, to, 0.75); 
+    // Vector3. Lerp works the same way as Mathf.Lerp except this one blends any given colors Like how the given numbers (4, 5, 6) reprent the floats of red, blue, green, and alpha
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+ 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f); 
+    // light.intensity increases the intensity of the light over the course of several frame starting at 0 all the way to 8 although this rate will be slow as it apporaches the target
+      
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    // light.intensity changes the intensity of the light per second rather then per frame
+
+
+    } 
+    */
+}
